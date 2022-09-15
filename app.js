fbd1ef5bf17d10ee3e97d752582b19cc74bf8a44
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-15 10:56:53 -0600

    add dom elements to js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 /* Imports */
 
 /* Get DOM Elements */
+const nameInput = document.getElementById('name-input');
+const biomeSelect = document.getElementById('biome-select');
+const castleSelect = document.getElementById('castle-select');
+const addAttraction = document.getElementById('add-attraction');
+const attractionInput = document.getElementById('attraction-input');
+const kingdomArticle = document.getElementById('kingdom-article');
+const kingdomDisplay = document.getElementById('kingdom-display');
+const biomeImage = document.getElementById('biome-image');
+const castleImage = document.getElementById('castle-image');
+const addButton = document.getAnimations('add-button');
 
 /* State */
 
