f1f431bc74c21c18c988b376f8e62a9326b5a920
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-15 11:52:46 -0600

    add functions and event listeners to js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/* Imports */
-
-/* Get DOM Elements */
 const nameInput = document.getElementById('name-input');
 const biomeSelect = document.getElementById('biome-select');
 const castleSelect = document.getElementById('castle-select');
@@ -12,10 +9,45 @@ const biomeImage = document.getElementById('biome-image');
 const castleImage = document.getElementById('castle-image');
 const addButton = document.getAnimations('add-button');
 
-/* State */
+const kingdom = {
+    name: 'Neogeo',
+    biome: 'forest',
+    castle: 'turkish',
+    attractions: [],
+};
 
 /* Events */
+nameInput.addEventListener('input', () => {
+    kingdom.name = nameInput.value;
+    displayKingdom();
+});
 
+biomeSelect.addEventListener('change', () => {
+    kingdom.biome = biomeSelect.value;
+    displayKingdom();
+});
+
+castleSelect.addEventListener('change', () => {
+    kingdom.castle = castleSelect.value;
+    displayKingdom();
+});
 /* Display Functions */
+function displayBuilder() {
+    nameInput.value = kingdom.name;
+    biomeSelect.value = kingdom.biome;
+    castleSelect.value = kingdom.castle;
+}
+
+function displayKingdom() {
+    kingdomArticle.classList.value = '';
+    kingdomArticle.classList.add(kingdom.biome, kingdom.castle);
+    kingdomDisplay.textContent = kingdom.name;
+    biomeImage.src = 'assets/biomes/' + kingdom.biome + '.jpeg';
+    biomeImage.alt = kingdom.biome;
+    castleImage.src = 'assets/castles/' + kingdom.castle + '.jpeg';
+    castleImage.alt = kingdom.castle;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayBuilder();
+displayKingdom();
